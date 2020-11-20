1f52f9ba0f5469b3e24d3425d67ad6f50520ab66
Komla Adzam <adzamkomla.dev@gmail.com>
2020-11-20 09:57:52 +0000

    add api versioning

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const configService = app.get(ConfigService);
-  const port = configService.get('app.port');
 
-  await app.listen(port);
+  app.setGlobalPrefix(configService.get('app.version'));
+  await app.listen(configService.get('app.port'));
 }
 bootstrap();
